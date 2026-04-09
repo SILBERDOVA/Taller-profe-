fb7a8d0b5f56874bfec17d7c9e318a354865430e
Daniel Correa <decorrea@unbosque.edu.co>
2026-04-09 13:56:14 +0000

    Declaración de constantes y variables

---
+++ index.js
@@ -0,0 +1,10 @@
+let nombrecompleto=""
+let edad= 0
+let tipodedocumento=""
+let numerodedocumento=""
+
+let salario=0;
+let comisiones=0;
+let totaldehorasextra=0;
+let clasificaiondenivelderisego=0;
+
