eb1fce39fff2b6588cc9ecc9b86c84d29a5fc2e4
Daniel Correa <decorrea@unbosque.edu.co>
2026-04-10 08:43:32 -0500

    que

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-let nombrecompleto=""
-let edad= 0
-let tipodedocumento=""
-let numerodedocumento=""
+// Ingreso de información básica 
+let nombreCompleto = "";
+let edad = 0;
+let tipoDocumento = "";
+let numeroDocumento = "";
 
-let salario=0;
-let comisiones=0;
-let totaldehorasextra=0;
-let clasificaiondenivelderisego=0;
+// Variables de entrada
+let salario = 0;
+let mesadaPensional = 0;
+let clasificacion = ""; 
+let pagoPension = 0;
+let prestacionesLey = 0;
+
+// Valores fijos
+const edad_minima = 18;
+const edad_maxima_beneficio = 25;
+const edad_jubilacion = 60;
+
+// Valores y Tarifas en Colombia para 2026
+const salario_minimo = 1750905;
+const salario_minimo_integral_vigente = 22761765;
+const subsidio_transporte = 249.095;
+const uvt = 52.37;
 
